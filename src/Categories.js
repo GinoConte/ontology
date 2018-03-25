8f9d749697cf6d3b86ae698f2b555914fd341cc1
GinoConte <gino@gamurs.com>
2018-03-26 09:43:02 +1100

    feat: add cards for categories page

--- src/Categories.js
+++ src/Categories.js
@@ -2,13 +2,45 @@ import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Col, Row, Container } from 'reactstrap'
 
+import {
+  Card,
+  CardActions,
+  CardHeader,
+  CardMedia,
+  CardTitle, 
+  CardText,
+} from 'material-ui/Card';
+import FlatButton from 'material-ui/FlatButton';
+
+
+import './App.css';
+
 class Categories extends Component {
   render() {
+    const categories = ['Housing prices', 'Education', 'Stock forecasting', 'Event study']
+    const renderedCategories = categories.map(category => (
+      <Col xs={4} className="Card-section">
+        <Card>
+          <CardTitle title={category} subtitle="Card subtitle" />
+          <CardText>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
+            Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
+            Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
+          </CardText>
+          <CardActions>
+            <FlatButton label="Start" />
+          </CardActions>
+        </Card>
+      </Col>
+    ));
     return (
-      <Container>
-        <Row>
+      <Container className="Categories">
+        <Row className="Cards-container">
           <Col xs={12}>
-            <h1>Hello</h1>
+            <Row>
+              {renderedCategories}
+            </Row>
           </Col>
         </Row>
       </Container>
