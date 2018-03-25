2c9e09318ef6d261b2171b74442a3ed72919d3ff
GinoConte <gino@gamurs.com>
2018-03-25 20:47:16 +1100

    feat: add bootstrap, add container page

---
+++ src/Categories.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Col, Row, Container } from 'reactstrap'
+
+class Categories extends Component {
+  render() {
+    return (
+      <Container>
+        <Row>
+          <Col xs={12}>
+            <h1>Hello</h1>
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
+}
+
+export default Categories;
