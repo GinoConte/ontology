b55cd206162925d633e79170b1242cdc1978eb99
GinoConte <gino@gamurs.com>
2018-03-26 23:30:09 +1100

    feat: add buttons to home page

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Col, Row, Container } from 'reactstrap'
 
+import FlatButton from 'material-ui/FlatButton';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 import './Home.css';
@@ -13,7 +14,14 @@ class Home extends Component {
         <Container className="Home">
           <Row>
             <Col xs={12}>
-                <h1>Financial Ontology</h1>
+              <h1>Financial Ontology</h1>
+            </Col>
+            <Col xs={12}>
+              <div className="Home-buttons-container">
+                <FlatButton label="About" />
+                <FlatButton href="/categories" label="Categories" />
+                <FlatButton label="Sign in" />
+              </div>
             </Col>
           </Row>
         </Container>
